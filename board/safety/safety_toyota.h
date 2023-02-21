ace1aead00f3af3ce27e4ac3155685b1e6cab534
ntegan <ntegan1@gmail.com>
2023-02-21 10:34:41 -0500

    10slat allow faster torque wind down

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -1,9 +1,9 @@
 const SteeringLimits TOYOTA_STEERING_LIMITS = {
   .max_steer = 1500,
-  .max_rate_up = 15,          // ramp up slow
-  .max_rate_down = 25,        // ramp down fast
-  .max_torque_error = 350,    // max torque cmd in excess of motor torque
-  .max_rt_delta = 450,        // the real time limit is 1800/sec, a 20% buffer
+  .max_rate_up = 17,          // ramp up slow
+  .max_rate_down = 32,        // ramp down fast
+  .max_torque_error = 600,    // max torque cmd in excess of motor torque
+  .max_rt_delta = 750,        // the real time limit is 1800/sec, a 20% buffer
   .max_rt_interval = 250000,
   .type = TorqueMotorLimited,
 
